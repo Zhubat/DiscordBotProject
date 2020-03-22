69b859361eaf8ec4dc80438db0eba767e8a4a232
Fan Zhu <55269121+Zhubat@users.noreply.github.com>
2020-03-22 21:03:57 +1100

    Add files via upload

--- commands/wallet/award.js
+++ commands/wallet/award.js
@@ -9,31 +9,40 @@ module.exports = {
     usage: "award",
     run: async (client, message, args, tools) => {
         if (message.member.roles.cache.some(role => role.name === 'Officer') || message.member.roles.cache.some(role => role.name === 'Guild Leader')){
-            //if no one mentioned to give
-            if (!message.mentions.members.first()) return message.channel.send(`**Please mention target!**\nUsage: +award <cp amount> <tag user here>`);
+            if (args.length != 2){
+                return message.channel.send(`**Invalid format!**\nUsage: +award <cp amount> <tag user here>`);
+            }
+            else{
+                //if no one mentioned to give
+                if (!message.mentions.members.first()) return message.channel.send(`**Please mention target!**\nUsage: +award <cp amount> <tag user here>`);
+                
+                //replaces targetMember in message with a number
+                let targetMember = message.mentions.members.first();
+                var isnum = /^\d+$/.test(args[0]);
 
-            //replaces targetMember in message with a number
-            let targetMember = message.mentions.members.first(),
-                amount = parseInt(args.join(' ').replace(targetMember, ''));
+                if (isnum){
+                
+                    let targetWallet = await db.fetch(`userBalance_${targetMember.id}`),
+                        selfWallet = await db.fetch(`userBalance_${message.author.id}`);
 
-            if (isNaN(amount)) return message.channel.send(`**Please enter a valid amount!**\nUsage: +award <cp amount> <tag user here>`);
+                    if (targetWallet === null) targetWallet = 0;
+                    if (selfWallet === null) selfWallet = 0;
 
-            let targetWallet = await db.fetch(`userBalance_${targetMember.id}`),
-                selfWallet = await db.fetch(`userBalance_${message.author.id}`);
-
-            if (targetWallet === null) targetWallet = 0;
-            if (selfWallet === null) selfWallet = 0;
-
-            
-            db.add(`userBalance_${targetMember.id}`, amount);
-            
-
-            message.channel.send(`**Congratulations! <@${targetMember.id}>\n You have being awarded ${amount}cp!**`);
-        }
+                    if (amount <= 0){
+                        return message.reply("Cannot have 0 or negative amounts!")
+                    }
+                    else{
+                        db.add(`userBalance_${targetMember.id}`, amount);
+                        message.channel.send(`**Congratulations! <@${targetMember.id}>\n You have been awarded ${amount}cp!**`);
+                    }
+                }
+                else{
+                    return message.channel.send(`**Please enter a valid amount!**\nUsage: +award <cp amount> <tag user here>`);
+                }
+            }
+        }      
         else{
             message.channel.send(`**Access Denied.**`)
         }
-        
-
     }
 }
--- commands/wallet/spend.js
+++ commands/wallet/spend.js
@@ -7,33 +7,45 @@ module.exports = {
     description: "take away cp",
     usage: "spend",
     run: async (client, message, args, tools) => {
-        const member = message.mentions.members.first();
         if (message.member.roles.cache.some(role => role.name === 'Officer') || message.member.roles.cache.some(role => role.name === 'Guild Leader')){
-            //if no one mentioned to give
-            if (!message.mentions.members.first()) return message.channel.send(`**Please mention target!**\nUsage: +take <cp amount> <tag user here>`);
-
-            //replaces targetMember in message with a number
-            let targetMember = message.mentions.members.first(),
-                amount = parseInt(args.join(' ').replace(targetMember, ''));
-
-            if (isNaN(amount)) return message.channel.send(`**Please enter a valid amount!**\nUsage: +take <cp amount> <tag user here>`);
-
-            let targetWallet = await db.fetch(`userBalance_${targetMember.id}`);
-
-            if (targetWallet === null) return message.reply("Nothing to take...");
-
-            if (amount > targetWallet){
-                return message.channel.send(`**Looks like they don't have that much...**`);
+            if (args.length != 2){
+                return message.channel.send(`**Invalid format!**\nUsage: +spend <cp amount> <tag user here>`);
             }
             else{
-                db.subtract(`userBalance_${targetMember.id}`, amount);
-            }
-            
-            message.channel.send(`**Successfully taken ${amount}cp from <@${targetMember.id}>!**`);
+                //if no one mentioned to give
+                if (!message.mentions.members.first()) return message.channel.send(`**Please mention target!**\nUsage: +spend <cp amount> <tag user here>`);
+                
+                //replaces targetMember in message with a number
+                let targetMember = message.mentions.members.first();
+                var isnum = /^\d+$/.test(args[0]);
 
-        }
+                if (isnum){
+                
+                    let targetWallet = await db.fetch(`userBalance_${targetMember.id}`),
+                        selfWallet = await db.fetch(`userBalance_${message.author.id}`);
+
+                    if (targetWallet === null) targetWallet = 0;
+                    if (selfWallet === null) selfWallet = 0;
+
+                    if (amount > targetWallet){
+                        return message.channel.send(`**Looks like they don't have that much...**`);
+                    }
+                    else if (amount <= 0){
+                        return message.reply("Cannot have 0 or negative amounts!")
+                    }
+                    else{
+                        db.subtract(`userBalance_${targetMember.id}`, amount);
+                        message.channel.send(`**<@${targetMember.id}>\n ${amount} has been deducted from your wallet.**`);
+                    }
+                }
+                else{
+                    return message.channel.send(`**Please enter a valid amount!**\nUsage: +spend <cp amount> <tag user here>`);
+                }
+            }
+        }      
         else{
             message.channel.send(`**Access Denied.**`)
         }
     }
 }
+
