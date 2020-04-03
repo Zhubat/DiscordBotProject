c85e8b847983e3af7c8cf97337cf15259b0d9e82
Fan Zhu <55269121+Zhubat@users.noreply.github.com>
2020-04-03 15:35:00 +1100

    Add files via upload

--- commands/pc/pc.js
+++ commands/pc/pc.js
@@ -5,44 +5,21 @@ module.exports = {
     description: "Price check items",
     usage: "pc",
     run: async (client, message, args) => {
+        var pc_dict = {"choiceband":"110-140k", "choicespecs":"110-140k", "choicescarf":"130-150k",
+                        "lifeorb": "100-120k", "assualtvest": "130-150k", "rerollticket":"\nIV: 600-700k\nNature: 350k",
+                        "rarecandy": "6-8k", "focussash": "3k", "toxicorb": "50k", "flameorb": "50k", "cc": "380-410k", 
+                        "blackmedalion": "180k", "bms": "180k", "membership": "\n15day: 200k\n30day:380k", "ms": "\n15day: 200k\n30day:380k",
+                        "razorclaw": "20k", "shinystone": "15-20k", "razorfang": "20k", "smokeball": "15-20k", "machobrace": "30k", 
+                        "protector": "20-30k", "deepseatooth": "20k", "deepseascale": "20k"}
         if (!args.length){
             return message.channel.send(`**No item written**\n+pc <item name>`);
         }
         else{
             item = args.join('');
             item = item.toLowerCase();
-            if (item === "choiceband" || item === "choicespecs"){
-                return message.reply("110k-140k");
-            }
-            else if(item === "choicescarf"){
-                return message.reply("130k-150k");
-            }
-            else if(item === "lifeorb"){
-                return message.reply("100k-120k");
-            }
-            else if(item === "assaultvest" || item === "rockyhelmet"){
-                return message.reply("130k-150k");
-            }
-            else if(item === "rerollticket"){
-                return message.reply("\nIV: 600k-700k\nNature: 350k");
-            }
-            else if(item === "rarecandy"){
-                return message.reply("6-8k");
-            }
-            else if(item === "focussash"){
-                return message.reply("3k");
-            }
-            else if(item === "toxicorb" || item === "flameorb"){
-                return message.reply("50k");
-            }
-            else if(item === "cc"){
-                return message.reply("380k-410k");
-            }
-            else if(item === "blackmedalion" || item === "bms"){
-                return message.reply("180k");
-            }
-            else if(item === "membership" || item === "ms"){
-                return message.reply("\n15day: 200k\n30day:380k");
+            
+            if (item in pc_dict){
+                return message.channel.send(`**${pc_dict[item]}**`)
             }
             else{
                 return message.channel.send(`**wrong format**\n+pc <item name>`);
