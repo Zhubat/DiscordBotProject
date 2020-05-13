ba199511ca073b21532f4be7264497fa57e58415
Fan Zhu <55269121+Zhubat@users.noreply.github.com>
2020-05-13 15:30:42 +1000

    Add files via upload

---
+++ commands/info/whoissss.js
@@ -0,0 +1,40 @@
+const Discord = require("discord.js");
+const { stripIndents } = require("common-tags");
+const { getMember, formatDate } = require("../../functions.js");
+
+module.exports = {
+    name: "whoissss",
+    aliases: ["who", "user", "info"],
+    description: "Returns user information",
+    usage: "[username | id | mention]",
+    run: (client, message, args) => {
+        const member = getMember(message, args.join(" "));
+
+        // Member variables
+        const joined = formatDate(member.joinedAt);
+        const roles = member.roles.cache
+            .filter(r => r.id !== message.guild.id)
+            .map(r => r).join(", ") || 'none';
+
+        // User variables
+        const exampleEmbed = new Discord.MessageEmbed()
+        .setColor('#0099ff')
+        .setTitle('Some title')
+        .setURL('https://discord.js.org/')
+        .setAuthor('Some name', 'https://i.imgur.com/wSTFkRM.png', 'https://discord.js.org')
+        .setDescription('Some description here')
+        .setThumbnail('https://i.imgur.com/wSTFkRM.png')
+        .addFields(
+            { name: 'Regular field title', value: 'Some value here' },
+            { name: '\u200B', value: '\u200B' },
+            { name: 'Inline field title', value: 'Some value here', inline: true },
+            { name: 'Inline field title', value: 'Some value here', inline: true },
+        )
+        .addField('Inline field title', 'Some value here', true)
+        .setImage('https://i.imgur.com/wSTFkRM.png')
+        .setTimestamp()
+        .setFooter('Some footer text here', 'https://i.imgur.com/wSTFkRM.png');
+
+        message.channel.send(exampleEmbed);
+    }
+}
